00d0414f53e7425302624e0f38a5c8ef1449ae15
Philippe Matray <phmatray@gmail.com>
2015-04-16 00:26:23 +0200

    Initial commit

---
+++ VGM.Web/obj/Release/Package/PackageTmp/js/app.js
@@ -0,0 +1,25 @@
+﻿var vgmApp = angular.module("vgmApp", ["ngSanitize", "firebase"]);
+
+vgmApp.controller("MainCtrl", function ($scope, $firebaseObject) {
+
+    var ref = new Firebase("https://vgmliege.firebaseio.com/");
+
+    // download the data into a local object
+    var obj = $firebaseObject(ref);
+
+    ////// to take an action after the data loads, use the $loaded() promise
+    ////obj.$loaded().then(function () {
+    ////    console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
+
+    ////    // To iterate the key/value pairs of the object, use angular.forEach()
+    ////    angular.forEach(obj, function (value, key) {
+    ////        console.log(key, value);
+    ////    });
+    ////});
+
+    // To make the data available in the DOM, assign it to $scope
+    $scope.data = obj;
+
+    // For three-way data bindings, bind it to the scope instead
+    obj.$bindTo($scope, "data");
+});
