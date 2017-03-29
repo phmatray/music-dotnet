9341b9889dfc7c886a455a1e39bb4acd437e9d19
Philippe Matray <phmatray@outlook.com>
2017-03-29 08:13:02 +0200

    WIP: Convert RadioButton to ButtonStyle

--- MidiMinuit/SamplePages/IntervalCalculator/IntervalCalculatorViewModel.cs
+++ MidiMinuit/SamplePages/IntervalCalculator/IntervalCalculatorViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace MidiMinuit.SamplePages.IntervalCalculator
+﻿namespace MidiMinuit.SamplePages.IntervalCalculator
 {
     using GalaSoft.MvvmLight;
     using Lib.Core.Intervals;
@@ -8,6 +6,7 @@ namespace MidiMinuit.SamplePages.IntervalCalculator
     using Lib.Core.NoteNames;
     using Lib.Core.Notes;
     using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
 
     public class IntervalCalculatorViewModel : ViewModelBase
     {
@@ -41,137 +40,65 @@ namespace MidiMinuit.SamplePages.IntervalCalculator
             set { Set(ref _interval, value); }
         }
 
-        private void RefreshNoteName(NoteName name)
+        public NoteName NoteNameC { get; } = new NoteNameC();
+        public NoteName NoteNameD { get; } = new NoteNameD();
+        public NoteName NoteNameE { get; } = new NoteNameE();
+        public NoteName NoteNameF { get; } = new NoteNameF();
+        public NoteName NoteNameG { get; } = new NoteNameG();
+        public NoteName NoteNameA { get; } = new NoteNameA();
+        public NoteName NoteNameB { get; } = new NoteNameB();
+
+        public NoteAccidental NoteAccidentalDoubleFlat { get; } = new NoteAccidentalDoubleFlat();
+        public NoteAccidental NoteAccidentalFlat { get; } = new NoteAccidentalFlat();
+        public NoteAccidental NoteAccidentalNatural { get; } = new NoteAccidentalNatural();
+        public NoteAccidental NoteAccidentalSharp { get; } = new NoteAccidentalSharp();
+        public NoteAccidental NoteAccidentalDoubleSharp { get; } = new NoteAccidentalDoubleSharp();
+
+        public Interval IntervalA2 { get; } = new IntervalAugmentedSecond();
+        public Interval IntervalA3 { get; } = new IntervalAugmentedThird();
+        public Interval IntervalA4 { get; } = new IntervalAugmentedFourth();
+        public Interval IntervalA5 { get; } = new IntervalAugmentedFifth();
+        public Interval IntervalA6 { get; } = new IntervalAugmentedSixth();
+        public Interval IntervalA7 { get; } = new IntervalAugmentedSeventh();
+        public Interval IntervalA8 { get; } = new IntervalAugmentedOctave();
+        public Interval IntervalM2 { get; } = new IntervalMajorSecond();
+        public Interval IntervalM3 { get; } = new IntervalMajorThird();
+        public Interval IntervalP4 { get; } = new IntervalPerfectFourth();
+        public Interval IntervalP5 { get; } = new IntervalPerfectFifth();
+        public Interval IntervalM6 { get; } = new IntervalMajorSixth();
+        public Interval IntervalM7 { get; } = new IntervalMajorSeventh();
+        public Interval IntervalP8 { get; } = new IntervalPerfectOctave();
+        public Interval IntervalMin2 { get; } = new IntervalMinorSecond();
+        public Interval IntervalMin3 { get; } = new IntervalMinorThird();
+        public Interval IntervalMin6 { get; } = new IntervalMinorSixth();
+        public Interval IntervalMin7 { get; } = new IntervalMinorSeventh();
+        public Interval IntervalDim2 { get; } = new IntervalDiminishedSecond();
+        public Interval IntervalDim3 { get; } = new IntervalDiminishedThird();
+        public Interval IntervalDim4 { get; } = new IntervalDiminishedFourth();
+        public Interval IntervalDim5 { get; } = new IntervalDiminishedFifth();
+        public Interval IntervalDim6 { get; } = new IntervalDiminishedSixth();
+        public Interval IntervalDim7 { get; } = new IntervalDiminishedSeventh();
+        public Interval IntervalDim8 { get; } = new IntervalDiminishedOctave();
+
+        public void NoteName_OnChecked(object sender, RoutedEventArgs e)
         {
-            NoteName = name;
-            var note = new Note(NoteName, NoteAccidental);
-            Interval.LowerNote = note;
+            NoteName = (sender as RadioButton)?.Tag as NoteName;
+            Interval.LowerNote = new Note(NoteName, NoteAccidental);
             RaisePropertyChanged(() => Interval);
         }
 
-        private void RefreshNoteAccidental(NoteAccidental accidental)
+        public void NoteAccidental_OnChecked(object sender, RoutedEventArgs e)
         {
-            NoteAccidental = accidental;
-            var note = new Note(NoteName, NoteAccidental);
-            Interval.LowerNote = note;
+            NoteAccidental = (sender as RadioButton)?.Tag as NoteAccidental;
+            Interval.LowerNote = new Note(NoteName, NoteAccidental);
             RaisePropertyChanged(() => Interval);
         }
 
-        private void RefreshInterval(Interval interval)
+        public void Interval_OnChecked(object sender, RoutedEventArgs e)
         {
+            var interval = (sender as RadioButton)?.Tag as Interval;
             interval.LowerNote = new Note(NoteName, NoteAccidental);
             Interval = interval;
         }
-
-        public void NoteNameC_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameC());
-
-        public void NoteNameD_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameD());
-
-        public void NoteNameE_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameE());
-
-        public void NoteNameF_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameF());
-
-        public void NoteNameG_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameG());
-
-        public void NoteNameA_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameA());
-
-        public void NoteNameB_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteName(new NoteNameB());
-
-        public void NoteAccidentalNatural_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteAccidental(new NoteAccidentalNatural());
-
-        public void NoteAccidentalFlat_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteAccidental(new NoteAccidentalFlat());
-
-        public void NoteAccidentalSharp_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteAccidental(new NoteAccidentalSharp());
-
-        public void NoteAccidentalDoubleFlat_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteAccidental(new NoteAccidentalDoubleFlat());
-
-        public void NoteAccidentalDoubleSharp_OnClick(object sender, RoutedEventArgs e)
-            => RefreshNoteAccidental(new NoteAccidentalDoubleSharp());
-
-        public void IntervalA2_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedSecond());
-
-        public void IntervalA3_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedThird());
-
-        public void IntervalA4_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedFourth());
-
-        public void IntervalA5_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedFifth());
-
-        public void IntervalA6_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedSixth());
-
-        public void IntervalA7_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedSeventh());
-
-        public void IntervalA8_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalAugmentedOctave());
-
-        public void IntervalM2_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMajorSecond());
-
-        public void IntervalM3_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMajorThird());
-
-        public void IntervalP4_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalPerfectFourth());
-
-        public void IntervalP5_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalPerfectFifth());
-
-        public void IntervalM6_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMajorSixth());
-
-        public void IntervalM7_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMajorSeventh());
-
-        public void IntervalP8_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalPerfectOctave());
-
-        public void IntervalMin2_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMinorSecond());
-
-        public void IntervalMin3_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMinorThird());
-
-        public void IntervalMin6_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMinorSixth());
-
-        public void IntervalMin7_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalMinorSeventh());
-
-        public void IntervalDim2_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedSecond());
-
-        public void IntervalDim3_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedThird());
-
-        public void IntervalDim4_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedFourth());
-
-        public void IntervalDim5_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedFifth());
-
-        public void IntervalDim6_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedSixth());
-
-        public void IntervalDim7_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedSeventh());
-
-        public void IntervalDim8_OnClick(object sender, RoutedEventArgs e)
-            => RefreshInterval(new IntervalDiminishedOctave());
     }
 }
